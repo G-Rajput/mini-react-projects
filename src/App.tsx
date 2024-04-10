8a8b01ffd98ff9d395dd36d181ec865228351e08
Govind Rajput <govindrajput9819@gmail.com>
2024-04-10 15:55:43 +0530

    Random Color Generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import Accordian from "./components/accordian/Accordian";
+import RandomColor from "./components/random-color/RandomColor";
 
 const App: React.FC = () => {
   return (
     <div className="main-container">
       {/* Accordian Component */}
-      <Accordian />
+      {/* <Accordian /> */}
+
+      {/* Random color Component */}
+      <RandomColor />
     </div>
   );
 };
